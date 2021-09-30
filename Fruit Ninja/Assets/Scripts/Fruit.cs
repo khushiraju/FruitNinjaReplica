1170bae24e249c5607f8500424aa26a95528409d
Khushi Raju <khushisurajraju@gmail.com>
2021-09-30 07:45:36 -0500

    chnages

--- Fruit Ninja/Assets/Scripts/Fruit.cs
+++ Fruit Ninja/Assets/Scripts/Fruit.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     public bool isActive{set; get;}
-
-    private float velocity;
+    private const float GRAVITY = 2.0f;
+    private float verticalVelocity;
     private float speed;
 
-    public void LaunchFruit(float velocity, float xSpeed, float xStart){
+    public void LaunchFruit(float verticalVelocity, float xSpeed, float xStart){
         isActive = true;
         this.velocity = velocity; 
         speed = xSpeed; 
 
         transform.position = new Vector3(xStart, 0, 0);
+    }
+
+    private void Update() {
+        velocity -= GRAVITY * Time.deltaTime;
     }
 }
