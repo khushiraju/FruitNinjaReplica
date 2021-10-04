318edaa92e9a898f7d10cf3e8cb2c720f7bbd21b
Khushi Raju <khushisurajraju@gmail.com>
2021-10-04 08:21:18 -0500

    oct 4 changes

--- Fruit Ninja/Assets/Scripts/GameManager.cs
+++ Fruit Ninja/Assets/Scripts/GameManager.cs
@@ -6,6 +6,20 @@ public class GameManager : MonoBehaviour
 {
     private List<Fruit> fruits = new List<Fruit>();
     public GameObject fruitPrefab;
+
+    private float lastSpawn; 
+    private float deltaSpawn = 2.0f;
+
+    private void Update(){
+        if (Time.time - lastSpawn > deltaSpawn){
+            Fruit f = GetFruit();
+            float randomX = Random.Range(-1.5f, 1.5f);
+            f.LaunchFruit(Random.Range(1.5f,3.0f), randomX,  -randomX);
+
+            lastSpawn = Time.time;
+        }
+    }
+
     private Fruit GetFruit(){
         Fruit f = fruits.Find(x => !x.isActive);
 
@@ -15,4 +29,6 @@ public class GameManager : MonoBehaviour
         }
         return f;
     }
+
+    // 22:31 
 }
