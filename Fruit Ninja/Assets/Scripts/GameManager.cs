592afb66a29e5282ad24cff75aa928ae1f6b7c81
Khushi Raju <khushisurajraju@gmail.com>
2021-12-10 07:56:00 -0600

    dec 10 changes before adding bombs

--- Fruit Ninja/Assets/Scripts/GameManager.cs
+++ Fruit Ninja/Assets/Scripts/GameManager.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private List<Fruit> fruits = new List<Fruit>();
+    //private List<Bomb> bombs = new List<Bomb>();
     public GameObject fruitPrefab;
+    //public GameObject bombPrefab;
     public Transform trail;
 
     public static int score = 0;
@@ -15,39 +17,41 @@ public class GameManager : MonoBehaviour
     private const float REQUIRED_SLICEFORCE = 10.0f;
     private Vector3 lastMousePos;
     private Collider2D[] fruitCols;
+    //private Collider2D[] bombCols;
 
     private void Start(){
         fruitCols = new Collider2D[0];
+        //bombCols = new Collider2D[0];
     }
 
     private void Update(){
-        if (Time.time - lastSpawn > deltaSpawn){
-            Fruit f = GetFruit();
-            float randomX = Random.Range(-1.65f, 1.65f);
-            f.LaunchFruit(Random.Range(1.85f,2.75f), randomX,  -randomX);
+            if ((Time.time - lastSpawn > deltaSpawn) && (Timer.timerIsRunning == true)){
+                Fruit f = GetFruit();
+                float randomX = Random.Range(-1.65f, 1.65f);
+                f.LaunchFruit(Random.Range(1.85f,2.75f), randomX,  -randomX);
+                lastSpawn = Time.time;
+            }
 
-            lastSpawn = Time.time;
-        }
+            if(Input.GetMouseButton(0)){
+                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                pos.z = -1;
+                trail.position = pos;
 
-        if(Input.GetMouseButton(0)){
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = -1;
-            trail.position = pos;
-
-            Collider2D[] thisFramesFruit = Physics2D.OverlapPointAll(new Vector2(pos.x, pos.y), LayerMask.GetMask("Fruit"));
-            
-            if((Input.mousePosition - lastMousePos).sqrMagnitude > REQUIRED_SLICEFORCE){ 
-                foreach(Collider2D c2 in thisFramesFruit){
-                    for(int i = 0; i < fruitCols.Length; i++){
-                        if (c2 == fruitCols[i]){
-                            c2.GetComponent<Fruit>().Slice();  
+                Collider2D[] thisFramesFruit = Physics2D.OverlapPointAll(new Vector2(pos.x, pos.y), LayerMask.GetMask("Fruit"));
+                
+                if((Input.mousePosition - lastMousePos).sqrMagnitude > REQUIRED_SLICEFORCE){ 
+                    foreach(Collider2D c2 in thisFramesFruit){
+                        for(int i = 0; i < fruitCols.Length; i++){
+                            if (c2 == fruitCols[i]){
+                                c2.GetComponent<Fruit>().Slice();  
+                            }
                         }
                     }
                 }
+                lastMousePos = Input.mousePosition;
+                fruitCols = thisFramesFruit;
             }
-            lastMousePos = Input.mousePosition;
-            fruitCols = thisFramesFruit;
-        }
+        
     }
 
     private Fruit GetFruit(){
@@ -59,5 +63,17 @@ public class GameManager : MonoBehaviour
         }
         return f;
     }
+
+    /**
+    private Bomb GetBomb(){
+        Fruit b = bombs.Find(x => !x.isActive);
+
+        if (b == null){
+            b = Instantiate(bombPrefab).GetComponent<Bomb>();
+            bombs.Add(b);
+        }
+        return b;
+    }
+    **/
 
 }
