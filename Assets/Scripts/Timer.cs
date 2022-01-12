2da52611a773c169e3f94e99aa684146ea9cf582
Khushi Raju <khushisurajraju@gmail.com>
2022-01-12 07:38:27 -0600

    project I can start with for second sem

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//https://gamedevbeginner.com/how-to-make-countdown-timer-in-unity-minutes-seconds/
+// used this link for this class
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 60;
+    public static bool timerIsRunning = false;
+    public Text timeText;
+
+    private void Start()
+    {
+        timeText = GetComponent<Text>();
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
