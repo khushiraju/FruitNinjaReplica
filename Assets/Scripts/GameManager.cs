bd08b30863f76faccc8ef631335a37e6759571df
Khushi Raju <khushisurajraju@gmail.com>
2022-01-27 08:03:28 -0600

    bombs are flying, before working on for loops

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,26 +6,34 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private List<Fruit> fruits = new List<Fruit>();
+    private List<Bomb> bombs = new List<Bomb>();
     public GameObject fruitPrefab;
+    public GameObject bombPrefab;
     public Transform trail;
-
     public static int score = 0;
-    private float lastSpawn; 
+    private float lastSpawn;
+    private float lastSpawnBomb;
     private float deltaSpawn = 1.0f;
     private const float REQUIRED_SLICEFORCE = 10.0f;
     private Vector3 lastMousePos;
     private Collider2D[] fruitCols;
+    private Collider2D[] bombCols;
 
     private void Start(){
         fruitCols = new Collider2D[0];
+        bombCols = new Collider2D[0];
     }
 
     private void Update(){
             if ((Time.time - lastSpawn > deltaSpawn) && (Timer.timerIsRunning == true)){
                 Fruit f = GetFruit();
+                Bomb b = GetBomb();
                 float randomX = Random.Range(-1.65f, 1.65f);
-                float randomX2 = Random.Range(-1.65f, 1.65f);
+                float randomX2 = Random.Range(-1.70f, 1.70f);
                 f.LaunchFruit(Random.Range(1.85f,2.75f), randomX,  -randomX);
+                if(Timer.timeRemaining % 13 == 0){
+                    b.LaunchBomb(Random.Range(1.85f,2.75f), randomX2,  -randomX2);
+                }
                 lastSpawn = Time.time;
             }
 
@@ -35,6 +43,7 @@ public class GameManager : MonoBehaviour
                 trail.position = pos;
 
                 Collider2D[] thisFramesFruit = Physics2D.OverlapPointAll(new Vector2(pos.x, pos.y), LayerMask.GetMask("Fruit"));
+                Collider2D[] thisFramesBomb = Physics2D.OverlapPointAll(new Vector2(pos.x, pos.y), LayerMask.GetMask("Fruit"));
 
                 if((Input.mousePosition - lastMousePos).sqrMagnitude > REQUIRED_SLICEFORCE){ 
                     foreach(Collider2D c2 in thisFramesFruit){
@@ -61,6 +70,16 @@ public class GameManager : MonoBehaviour
             fruits.Add(f);
         }
         return f;
+    }
+
+    private Bomb GetBomb(){
+        Bomb b = bombs.Find(x => !x.isActive);
+
+        if (b == null){
+            b = Instantiate(bombPrefab).GetComponent<Bomb>();
+            bombs.Add(b);
+        }
+        return b;
     }
 
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timeRemaining = 60;
+    public static float timeRemaining = 60;
     public static bool timerIsRunning = false;
     public Text timeText;
 
